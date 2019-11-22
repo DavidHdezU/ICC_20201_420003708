439c77abe8232b922dbc40ef2b5934aeac40f013
David Hernández Uriostegui <Dhdezu@ciencias.unam.mx>
2019-11-22 08:34:50 -0600

    Resubiendo Practica03...

--- practica03/HeyJude.java
+++ practica03/HeyJude.java
@@ -1,77 +1,68 @@
 public class HeyJude{
-    
-    public static void main(String[] args){
-	String HEY = "Hey Jude ";
-	String DON = "don't ";
-	String MAK = "make it bad.";
-	String BEA = "be afraid.";
-	String LET = "let me down.";
-	String TAK = "Take a sad song and make it better.";
-	String YOW = "You were made to go out and get her.";
-	String YOH = "You have found her, now go and get her.";
-	String REM = "Remember to ";
-	String LEH = "let her into you heart.";
-	String LES = "let her into your skin.";
-	String THE = "Then you ";
-	String CAN = "can start ";
-	String BEG = "begin ";
-	String TOM = "to make it better ";
-	String BET = "better ";
-	String NA  = "na ";
-	String THEMIN = "The minute ";
-	String AND = "And anytime you feel the pain, hey Jude, refrain,\n"
-	    + "Don’t carry the world upon your shoulders.\n"
-	    + "For well you know that it’s a fool who plays it cool\n"
-	    + "By making his world a little colder.";
-	String SOL = "So let it out and let it in, hey Jude, begin,\n"
-	    + "You’re waiting for someone to perform with.\n"
-	    + "And don’t you know that it’s just you, hey jude, you’ll do,\n"
-	    + "The movement you need is on your shoulder.";
+	public static String HEY = "Hey Jude ";
+	public static String DON = "don't ";
+	public static String MAK = "make it bad.";
+	public static String BEA = "be afraid.";
+	public static String LET = "let me down.";
+	public static String TAK = "Take a sad song and make it better.";
+	public static String YOW = "You were made to go out and get her.";
+	public static String YOH = "You have found her, now go and get her.";
+	public static String REM = "Remember to ";
+	public static String LEH = "let her into you heart.";
+	public static String LES = "let her into your skin.";
+	public static String THE = "Then you ";
+	public static String CAN = "can start ";
+	public static String BEG = "begin ";
+	public static String TOM = "to make it better ";
+	public static String BET = "better ";
+	public static String NA  = "na ";
 
-	
-        String[] coro = {HEY, DON, MAK, TAK, REM, LEH, THE, CAN, TOM};
-	String[] p1 = {HEY, DON, BEA, YOW,THEMIN, LES, THE, BEG, TOM};
-	String[] p2 = {AND}; // NA seis veces
-	String[] p3 = {HEY, DON, LET, YOH, REM, LEH, THE, CAN, TOM}; 
-	String[] p4 = {SOL, NA, NA, NA, NA, NA, NA}; // NA seis veces 
-	String[] p5 = {HEY, DON, MAK, TAK, REM, LES, THE, BEG, TOM}; // better cinco veces
-	String[] p6 = {NA,NA ,NA, NA, NA, NA, NA, HEY}; // Repetir dos veces (NA 9 veces, HEY)
-	
-	String[][] parrafos = {coro, p1, p2, p3, p4, p5, p6};
+	public static String AND = "And anytime you feel the pain, hey Jude, refrain,\n"
+								+ "Don’t carry the world upon your shoulders.\n"
+									+ "For well you know that it’s a fool who plays it cool\n"
+								+ "By making his world a little colder.";
 
-	for(int i = 0; i < parrafos.length; i++){
-	    if(i < 3)
-		imprimirParrafo(parrafos[i], 1);
-	    else
-		imprimirParrafo(parrafos[i], 2); 
-	}
-     
-    }
-     //Parámetros del método: String[], el numerito del switch                                                                     
-     public static void imprimirParrafo(String[] parrafo, int numerito){
-         switch(numerito){
-	     case 1:
-		 System.out.print("\n");
-		 int a = 0;
-		 while(a < parrafo.length){
-		     if(parrafo[a].charAt(parrafo[a].length() -1) != '.')
-			 System.out.print(parrafo[a]);
-		     else
-			 System.out.print(parrafo[a]+"\n");
-		     a++;
-		 }
-		 break;
+	public static String SOL = "So let it out and let it in, hey Jude, begin,\n"
+								+ "You’re waiting for someone to perform with.\n"
+								+ "And don’t you know that it’s just you, hey jude, you’ll do,\n"
+								+ "The movement you need is on your shoulder.";
 
-	       case 2:
-		 System.out.print("\n");
-	         for(int i = 0; i < parrafo.length; i++){
-		     if(parrafo[i].charAt(parrafo[i].length() -1) != '.')
-			 System.out.print(parrafo[i]);
-		     else
-			 System.out.print(parrafo[i]+"\n");
-		 }	 
-		 break;
-	 }
-     }
-
+	public static void main(String[] args){
+		int j=0;
+		for (int i= 1;i<13;i++){
+			System.out.print(HEY+DON);
+			switch(i%3){
+				case 1: System.out.println(MAK);
+					System.out.println(TAK);
+					break;
+				case 2:System.out.println(BEA+YOW);
+					break;
+				case 3:System.out.println(LET+YOH);
+					break;
+			}
+			System.out.print(REM);
+			if(i%2==0){
+				System.out.println(LEH);
+			}
+			else System.out.println(LES);
+			System.out.print(THE);
+			if (i%2==0){
+				System.out.print(CAN);
+			}
+			else System.out.print(BEG);
+			System.out.println(TOM);
+			while(j!=5){
+				System.out.print(BET);
+				j++;
+			}
+			System.out.println("waaaaa");
+			j=0;
+			while(j!=5){
+				System.out.print(NA);
+				j++;
+			}
+			System.out.println("");
+			j=0;
+		}
+	}   
 }
